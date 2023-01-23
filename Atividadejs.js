47488a3425b47ba886490905c2421a82fc26bf39
Filipe <lipemag7@hotmail.com>
2023-01-23 09:29:46 -0300

    Segundo Commit

---
+++ Atividadejs.js
@@ -0,0 +1,22 @@
+function question(pergunta, certa, errada1, errada2){
+    this.pergunta = pergunta
+    this.certa = certa
+    this.errada1 = errada1 
+    this.errada2 = errada2
+}
+// window.onload = function (){
+//     var pergunta = document.getElementById("pergunta")
+//     pergunta.innerHTML = ''
+// }
+function clicou(){
+    
+
+}
+var perguntas = [
+    {
+        pergunta: "Qual o nome do pai do naruto?"
+        
+    }
+]
+var pergunta = document.getElementById("pergunta")
+    pergunta.innerHTML = 'dale'
