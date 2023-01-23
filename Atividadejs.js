1ed0919aa5c51d7ccf9a78cbaded4dbe84c6aea0
Filipe <lipemag7@hotmail.com>
2023-01-23 12:29:03 -0300

    Tudo estável

--- Atividadejs.js
+++ Atividadejs.js
@@ -4,18 +4,33 @@ function question(pergunta, certa, errada1, errada2){
     this.errada1 = errada1 
     this.errada2 = errada2
 }
-// window.onload = function (){
-//     var pergunta = document.getElementById("pergunta")
-//     pergunta.innerHTML = ''
-// }
+window.onload = function (){
+    // var pergunta = document.getElementById("pergunta")
+    // pergunta.innerHTML = ''
+}
 function clicou(){
-    
+    var pergunta = document.getElementById("pergunta")
+    var slot1 = document.getElementById("resposta1")
+    var resposta2 = document.getElementById("resposta2")
+    var slot3 = document.getElementById("resposta3")
+    pergunta.innerHTML = ""
+    slot1.innerHTML = ""
+    resposta2.innerHTML = ""
+    slot3.innerHTML = ""
+    pergunta.innerHTML = perguntas[0].pergunta
+    slot1.innerHTML = perguntas[0].certa
+    resposta2.innerHTML = perguntas[0].errada1
+    slot3.innerHTML = perguntas[0].errada2
 
 }
+
+
 var perguntas = [
     {
-        pergunta: "Qual o nome do pai do naruto?"
-        
+        pergunta: "Qual o nome do pai do naruto?",
+        certa: 'Minato',
+        errada1: "Kakashi",
+        errada2:"Nimato"
     }
 ]
 var pergunta = document.getElementById("pergunta")
